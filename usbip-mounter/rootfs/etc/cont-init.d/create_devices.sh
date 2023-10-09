1964700ad85849bbe8133d55bb5f2c4eb67a8ce8
Ilya Rakhlin <i.rakhlin@gmail.com>
2023-10-09 00:24:00 -0400

    update image

--- usbip-mounter/rootfs/etc/cont-init.d/create_devices.sh
+++ usbip-mounter/rootfs/etc/cont-init.d/create_devices.sh
@@ -9,9 +9,12 @@ declare server_address
 declare bus_id
 declare script_directory
 declare mount_script
+declare unmount_script
+declare counter
 
 script_directory="/usr/local/bin"
 mount_script="/usr/local/bin/mount_devices"
+unmount_script="/usr/local/bin/unmount_devices"
 
 if ! bashio::fs.directory_exists "${script_directory}"; then
   bashio::log.info  "Creating script directory"
@@ -20,6 +23,10 @@ fi
 
 if bashio::fs.file_exists "${mount_script}"; then
   rm "${mount_script}"
+fi
+
+if bashio::fs.file_exists "${unmount_script}"; then
+  rm "${unmount_script}"
 fi
 
 if ! bashio::fs.file_exists "${mount_script}"; then
@@ -33,5 +40,18 @@ if ! bashio::fs.file_exists "${mount_script}"; then
     bus_id=$(bashio::config "devices[${device}].bus_id")
     bashio::log.info "Adding device from server ${server_address} on bus ${bus_id}"
     echo "/usr/sbin/usbip --debug attach -r ${server_address} -b ${bus_id}" >> "${mount_script}"
+  done
+fi
+
+counter=0
+if ! bashio::fs.file_exists "${unmount_script}"; then
+  touch ${unmount_script}
+  chmod +x ${unmount_script}
+  echo '#!/command/with-contenv bashio' > "${unmount_script}"
+  echo 'set -x' >> "${unmount_script}"
+  for device in $(bashio::config 'devices|keys'); do
+    bashio::log.info "Adding detach for device on port ${counter}"
+    echo "/usr/sbin/usbip detach -p ${counter}" >> "${unmount_script}"
+    counter=$((counter+1))
   done
 fi
